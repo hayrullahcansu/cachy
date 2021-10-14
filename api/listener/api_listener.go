94431c1d0f3876c26960a399963e325e0dd76911
ertugrul-k <ertugrul@finedinemenu.com>
2021-10-14 18:11:18 +0300

    Update api_listener.go

--- api/listener/api_listener.go
+++ api/listener/api_listener.go
@@ -27,11 +27,7 @@ func (a *ApiListener) ListenAndServe() {
 	// a.mux.HandleFunc("/api/v1/hello", HelloServer)
 	a.mux.HandleFunc("/api/v1/cache/", routing.CacheRouteHandler)
 	// [START setting_port]
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
+	port := "8080"
 	log.Printf("Listening on port %s", port)
 	uri := ":" + port
 	http.ListenAndServe(uri, a.mux)
